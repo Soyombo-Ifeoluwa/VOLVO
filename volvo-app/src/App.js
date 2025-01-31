d2af305ce2d7ed1ee1d20ffefe6752d6b7616656
Ifeoluwa <soyomboifeoluwa90@gmail.com>
2025-01-31 21:45:23 +0100

    initial commit

--- volvo-app/src/App.js
+++ volvo-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import app from './Group 414.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero">
+      <img src={logo} alt="React Image" />
+      <div className="section1">
+      <div className="left">
+        <h3> Your car is in your pocket</h3>
+        <h1>CONTROL,</h1>
+        <h1>CONNECT,</h1>
+        <h1>DRIVE.</h1>
+        <h3>Experience the future of driving with<br/> seamless control and connectivity.</h3>
+        <div className="button">
+        <button className="button1">DOWNLOAD ON APP STORE</button>
+        <button className="button2">DOWNLOAD ON PLAY STORE </button>
+        </div>
+
+      </div>
+      <div className="right">
+      <img src={app} alt="img" />    
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
